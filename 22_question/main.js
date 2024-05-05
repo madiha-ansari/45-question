ee39dece52e02d269a1114c9807940bc7369570a
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-05-05 21:02:33 +0500

    add question no 22

---
+++ 22_question/main.js
@@ -0,0 +1,6 @@
+// creating a arrary
+var errorArrary = ["A", "B", "C", "D"];
+// producing error! by accessing invalid index of arrary
+// console.log(errorArrary[10]);
+// error removed 
+console.log(errorArrary[1]);
---
+++ 22_question/main.ts
@@ -0,0 +1,8 @@
+// creating a arrary
+let errorArrary :string[] = ["A","B","C","D"];
+
+// producing error! by accessing invalid index of arrary
+// console.log(errorArrary[10]);
+
+// error removed 
+console.log(errorArrary[1]);
