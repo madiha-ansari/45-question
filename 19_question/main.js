d62c656fe861acc5570732a759ae03151d04878b
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-05-05 17:56:03 +0500

    add the question no 19

---
+++ 19_question/main.js
@@ -0,0 +1,4 @@
+var guestList = ["hamza", "ali", "huzaifa", "bilal", "sonia"];
+// guestList.forEach(oneguest => console.log(`Salam ${oneguest}, would you like to dinner me?`));
+var lengthGuests = guestList.length;
+console.log("We are Inviting total ".concat(lengthGuests, "guest."));
---
+++ 19_question/main.ts
@@ -0,0 +1,8 @@
+
+let guestList = ["hamza","ali","huzaifa","bilal","sonia"];
+
+// guestList.forEach(oneguest => console.log(`Salam ${oneguest}, would you like to dinner me?`));
+
+let lengthGuests :number = guestList.length;
+
+console.log(`We are Inviting total ${lengthGuests}guest.`);
