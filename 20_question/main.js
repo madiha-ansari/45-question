ec6cd5421bb1ed8f515775b4079dc22f6434b484
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-05-05 18:21:50 +0500

    add question no 20

---
+++ 20_question/main.js
@@ -0,0 +1,7 @@
+// making a programming arrary 
+var programmingLanguages = ["Typescript", "Javascript", "Phyton", "PHP"];
+// print the message of list
+console.log("list of programming Languages:");
+// print the value of arrary in the form of list
+programmingLanguages.forEach(function (languages) { return console.log(languages); });
+// console.log(programmingLanguages);
---
+++ 20_question/main.ts
@@ -0,0 +1,10 @@
+// making a programming arrary 
+let programmingLanguages: string[] = ["Typescript","Javascript","Phyton","PHP"];
+
+// print the message of list
+console.log("list of programming Languages:");
+
+// print the value of arrary in the form of list
+programmingLanguages.forEach(languages => console.log(languages));
+
+// console.log(programmingLanguages);
